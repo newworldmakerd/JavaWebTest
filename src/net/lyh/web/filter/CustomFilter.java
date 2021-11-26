dd60e4d55542ba6274916bb6ae5ad4d5412b249f
lyh <1371850283@qq.com>
2021-11-26 20:14:43 +0800

    Filter练习2

--- src/net/lyh/web/filter/CustomFilter.java
+++ src/net/lyh/web/filter/CustomFilter.java
@@ -6,16 +6,17 @@ import java.io.IOException;
 public class CustomFilter implements Filter {
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
-
+        System.out.println("init");
     }
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-
+        System.out.println("dofilter");
+        filterChain.doFilter(servletRequest,servletResponse);
     }
 
     @Override
     public void destroy() {
-
+        System.out.println("destroy");
     }
 }
